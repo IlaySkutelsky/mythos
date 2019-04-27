999f6a250f006243c7352c811ffd1b8973070a03
Ilay Skutelsky <ilay.skutelsky@gmail.com>
2019-04-27 19:01:53 +0300

    heroku build

---
+++ precache-manifest.3e2cfa0615c3ffceb5e271366873d089.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "1812ee68b950988d46f61398093b5a4d",
+    "url": "/<mythos>/img/facebook.1812ee68.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/<mythos>/robots.txt"
+  },
+  {
+    "revision": "95fef60db00fbb1dec63",
+    "url": "/<mythos>/js/chunk-vendors.47350325.js"
+  },
+  {
+    "revision": "59dc783d48a70e14768c",
+    "url": "/<mythos>/js/app.14b3de58.js"
+  },
+  {
+    "revision": "e3265da0b912786c641c",
+    "url": "/<mythos>/js/about.69260e02.js"
+  },
+  {
+    "revision": "edb4a0f0de2f9964424b5765cd160402",
+    "url": "/<mythos>/index.html"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/<mythos>/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "963d1698fffd7b5f6af4db4df3296443",
+    "url": "/<mythos>/img/instagram.963d1698.svg"
+  },
+  {
+    "revision": "8f8d86957eed8230065befbcef336061",
+    "url": "/<mythos>/img/head.8f8d8695.jpg"
+  },
+  {
+    "revision": "51c0182d6401097a9cd504f98dc5afb8",
+    "url": "/<mythos>/img/envelope.51c0182d.svg"
+  },
+  {
+    "revision": "f316496cd2fde6ae82fbf9f6a7cb5122",
+    "url": "/<mythos>/img/arpilit-logo.f316496c.png"
+  },
+  {
+    "revision": "59dc783d48a70e14768c",
+    "url": "/<mythos>/css/app.8e03b67e.css"
+  }
+];
