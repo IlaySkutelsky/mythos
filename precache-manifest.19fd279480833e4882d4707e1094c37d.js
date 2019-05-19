c57119f668d18d348a0dc207503ef23e0eb10b5c
Ilay Skutelsky <ilay.skutelsky@gmail.com>
2019-05-19 22:23:04 +0300

    improved (team, plot, location)

---
+++ precache-manifest.19fd279480833e4882d4707e1094c37d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "1812ee68b950988d46f61398093b5a4d",
+    "url": "/img/facebook.1812ee68.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "51c0182d6401097a9cd504f98dc5afb8",
+    "url": "/img/envelope.51c0182d.svg"
+  },
+  {
+    "revision": "57cf097931a13bae6654",
+    "url": "/js/app.353f1fa4.js"
+  },
+  {
+    "revision": "95fef60db00fbb1dec63",
+    "url": "/js/chunk-vendors.47350325.js"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "e3265da0b912786c641c",
+    "url": "/js/about.69260e02.js"
+  },
+  {
+    "revision": "4508e826428dd8b66c54d014b9bdc515",
+    "url": "/index.html"
+  },
+  {
+    "revision": "128104e165be47a792567aa39f949b23",
+    "url": "/img/smadar.128104e1.jpg"
+  },
+  {
+    "revision": "26186585bf11cce6126943bded1c9a99",
+    "url": "/img/orgad.26186585.png"
+  },
+  {
+    "revision": "963d1698fffd7b5f6af4db4df3296443",
+    "url": "/img/instagram.963d1698.svg"
+  },
+  {
+    "revision": "57d11eeee50cd8b359b38917f52f435a",
+    "url": "/img/galit.57d11eee.jpg"
+  },
+  {
+    "revision": "ccdba01cd1a417d910a685e7c8c28d1a",
+    "url": "/img/eran.ccdba01c.jpg"
+  },
+  {
+    "revision": "55ebc263bbbfa167728f150d77820818",
+    "url": "/img/assaf.55ebc263.jpg"
+  },
+  {
+    "revision": "f316496cd2fde6ae82fbf9f6a7cb5122",
+    "url": "/img/arpilit-logo.f316496c.png"
+  },
+  {
+    "revision": "57cf097931a13bae6654",
+    "url": "/css/app.466b7f5e.css"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f67345b05df2759e459aedd3e5218549.js"
+  "/precache-manifest.19fd279480833e4882d4707e1094c37d.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "smadar-one-pager"});
